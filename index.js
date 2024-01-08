4a60fdbcf43d353037a4e71e65884c87c83ecc79
lepetoc <lepetoctgp@outlook.fr>
2024-01-08 18:01:43 +0100

    Added connection to database

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+require('dotenv').config()
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_BASE
+})
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/', async (req, res) => {
+    let conn;
+  try {
+    conn = await pool.getConnection();
+    res.send("Hello World!");
+  } catch (error) {
+    res.status(500).send("An error occurred");
+  } finally {
+    if (conn) conn.release();
+  }
 });
 
-app.listen(2000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log('Server is running on port ' + process.env.PORT);
 });
