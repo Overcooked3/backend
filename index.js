dc0d60198e145f662f2ab4585746a84d7bb0e8ee
AROLLAND1 <alexandre.rolland85@ynov.com>
2024-01-16 11:12:02 +0100

    ✨Last opt to begin the back

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
+  // const users = await prisma.user.findMany();
+  res.json({});
 });
 
 app.listen(process.env.PORT, () => {
