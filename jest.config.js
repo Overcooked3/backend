27f217d46d6371d5a9f55a33f162083ad0f069a7
Lisachard <lisa.achard@ynov.com>
2024-01-16 17:14:24 +0100

    �  Fixing minor issues with config files

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  clearMocks: true,
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
-  setupFilesAfterEnv: ["<rootDir>/singleton.ts"],
-};
